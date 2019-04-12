fa3c718bd09915e9040ba93516d1b582a73aa139
Chao Yang <chao.yang@fairfaxmedia.co.nz>
2019-04-12 16:48:44 +1200

    clean up

--- js/client-cookie-trace.js
+++ js/client-cookie-trace.js
@@ -1,28 +1,4 @@
 // document.cookie
-const parseCookie = function(cookie) {
-	const c = cookie.split(';').reduce((res, c) => {
-		const [key, val] = c.trim().split('=').map(decodeURIComponent)
-		const allNumbers = str => /^\d+$/.test(str);
-		try {
-			return Object.assign(res, { [key]: allNumbers(val) ?  val : JSON.parse(val) })
-		} catch (e) {
-			return Object.assign(res, { [key]: val })
-		}
-	}, {})
-
-	const name = Object.keys(c).filter((name) => !['domain', 'path', 'max-age', 'expires', 'secure', 'samesite', 'http-only'].includes(name))[0];
-	const value = c[name]
-	delete c[name]
-	delete c['']
-	return {
-		...c,
-		name,
-		value
-	}
-}
-
-// const background = chrome.extension.getBackgroundPage()
-
 const cookie_setter = document.__lookupSetter__('cookie').bind(document);
 const cookie_getter = document.__lookupGetter__('cookie').bind(document);
 
